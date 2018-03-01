cfe1caebec3df30440426aaea358b0baf1f16140
joH1 <jiema@live.fr>
2018-03-01 17:11:40 +0100

    Tidy up source
    Add linebreaks between funcs, de-inline short funcs declarations, move
    `static` funcs declarations to the beginning of the file.

--- log.c
+++ log.c
@@ -5,15 +5,12 @@
 #include <time.h> /* for time_t, time, strftime, localtime */
 
 
-static FILE *logfile = NULL; /* cannot initialize to stderr :'( gcc says
-                                "error: initializer element is not constant" */
 
+static FILE *logfile = NULL; /* cannot initialize to stderr :'( error is
+                                "initializer element is not constant" */
 static const char *header = NULL;
-
 static const char *timefmt = "%c";
-
 static LogLevel logfilter = LOGF_ALL;
-
 static const char* headers[] = {
 		[LOG_DEBUG] = "DEBUG",
 		[LOG_VERBOSE] = "VERBOSE",
@@ -25,6 +22,16 @@ static const char* headers[] = {
 };
 
 
+static inline void printheader(FILE *const out) {
+	static char date[32] = {0};
+	time_t t = time(NULL);
+	strftime(date + 1, 30, timefmt, localtime(&t));
+	fprintf(out, "%s %s\n", header, date);
+}
+
+static inline bool msgblank(const char *const msg) {
+	return *msg == '\0' || (isspace(*msg) && msgblank(msg + 1));
+}
 
 
 void log_setlogfile(FILE *const f) {
@@ -42,27 +49,27 @@ void log_setheader(const char *const value) {
 	*/
 }
 
-void log_setfilter(const LogLevel lvl) { logfilter = lvl; }
-LogLevel log_getfilter(void) { return logfilter; }
+void log_setfilter(const LogLevel lvl) {
+	logfilter = lvl;
+}
+
+LogLevel log_getfilter(void) {
+	return logfilter;
+}
 
 const char *log_getfiltername(void) {
 	return headers[logfilter];
 }
 
-void log_settimefmt(const char *const fmt) { timefmt = fmt; }
-const char *log_gettimefmt(void) { return timefmt; }
 
-
-static inline void printheader(FILE *const out) {
-	static char date[32] = {0};
-	time_t t = time(NULL);
-	strftime(date + 1, 30, timefmt, localtime(&t));
-	fprintf(out, "%s %s\n", header, date);
+void log_settimefmt(const char *const fmt) {
+	timefmt = fmt;
 }
 
-static bool msgblank(const char *const msg) {
-	return *msg == '\0' || (isspace(*msg) && msgblank(msg + 1));
+const char *log_gettimefmt(void) {
+	return timefmt;
 }
+
 
 void vmlog(const LogLevel lvl, const char *const fmt, va_list a) {
 	/* Initialize logfile if it has not been already */
