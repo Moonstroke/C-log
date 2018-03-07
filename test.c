b24ad2668b3323bba98162bcd3d1783c2b647f8c
joH1 <jiema@live.fr>
2018-03-07 16:05:25 +0100

    Update filter funcs in test
    The functions `log_getfilterlevel` and `log_setfilterlevel` are
    correctly called from the test function instead of their former names.

--- test.c
+++ test.c
@@ -30,11 +30,11 @@ int main(void) {
 	testlog("OK\n\n");
 
 	testlog("test log_setfilter(%d)\n", lvl);
-	log_setfilter(lvl);
+	log_setfilterlevel(lvl);
 	testlog("OK\n\n");
 
 	testlog("test log_getilter() == %d\n", lvl);
-	assert(log_getfilter() == lvl);
+	assert(log_getfilterlevel() == lvl);
 	testlog("OK\n\n");
 
 	testlog("test log_getfiltername() == \"%s\"\n", lname);
