015c647c06fac1b8c409d5dc6c8f30b5454c968e
joH1 <jiema@live.fr>
2018-03-08 14:32:43 +0100

    Combine headers
    The *attributes* header file `log_attrs.h` is moved within the main
    header, `log.h`, at the beginning of the file.

--- inc/log.h
+++ inc/log.h
@@ -36,7 +36,33 @@
 #include <stdarg.h> /* for va_* */
 #include <stdio.h> /* for FILE, fprintf, fputs */
 
-#include "log_attrs.h" /* for function attributes */
+/* ## FUNCTION ATTRIBUTES ## */
+
+#ifndef __GNUC__
+# define __attribute__(x) /**< \brief Empty definition for ISO C. */
+#endif
+
+/**
+ * \brief The arguments at given indices (starting from \c 1) must not be
+ *        \c NULL pointers.
+ *
+ * \param[in] ... The arguments to check for non-\c NULL value.
+ */
+#define NOTNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
+
+/** \brief The function does not affect global data. */
+#define PURE __attribute__((__pure__, __warn_unused_result__))
+
+/** \brief The function call is replaced by its code when encountered. */
+#define INLINE __attribute__((__always_inline__)) inline
+
+/**
+ * \brief The function uses a \a printf -like formatting style.
+ *
+ * \param[in] fmt  The index of the format string arguments
+ * \param[in] arg1 The index of the first format argument
+ */
+#define PRINTF(fmt, arg1) __attribute__((__format__(printf, fmt, arg1)))
 
 
 /**
